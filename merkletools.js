c819b04473b64ae052a544fae71697c64f4d50df
Jason Bukowski <jason@tierion.com>
2016-06-29 21:20:29 -0400

    Updated variable/method structures and added tests

--- merkletools.js
+++ merkletools.js
@@ -16,139 +16,157 @@ var MerkleTools = function (treeOptions) {
             hashType = treeOptions.hashType;
         }
     }
-    this.hashFunction = function (value) {
+    var hashFunction = function (value) {
         return crypto.createHash(hashType).update(value).digest();
     };
-    this.tree = {};
-    this.tree.leaves = [];
-    this.tree.levels = [];
-    this.tree.isReady = false;
-};
+    var tree = {};
+    tree.leaves = [];
+    tree.levels = [];
+    tree.isReady = false;
 
-////////////////////////////////////////////
-// Primary functions
-////////////////////////////////////////////
+    ////////////////////////////////////////////
+    // Public Primary functions
+    ////////////////////////////////////////////
 
-// Resets the current tree to empty
-MerkleTools.prototype.resetTree = function () {
-    this.tree = {};
-    this.tree.leaves = [];
-    this.tree.levels = [];
-    this.tree.isReady = false;
-};
+    // Resets the current tree to empty
+    this.resetTree = function () {
+        tree = {};
+        tree.leaves = [];
+        tree.levels = [];
+        tree.isReady = false;
+    };
 
-// Add a leaf to the tree
-// Accepts hash value as a Buffer or hex string
-MerkleTools.prototype.addLeaf = function (value) {
-    this.tree.isReady = false;
-    this.tree.leaves.push(this._getBuffer(value));
-};
+    // Add a leaf to the tree
+    // Accepts hash value as a Buffer or hex string
+    this.addLeaf = function (value) {
+        tree.isReady = false;
+        tree.leaves.push(_getBuffer(value));
+    };
 
-// Add a leaves to the tree
-// Accepts hash values as an array of Buffers or hex strings
-MerkleTools.prototype.addLeaves = function (valuesArray) {
-    this.tree.isReady = false;
-    var that = this;
-    valuesArray.forEach(function (value) {
-        that.tree.leaves.push(that._getBuffer(value));
-    });
-};
+    // Add a leaves to the tree
+    // Accepts hash values as an array of Buffers or hex strings
+    this.addLeaves = function (valuesArray) {
+        tree.isReady = false;
+        valuesArray.forEach(function (value) {
+            tree.leaves.push(_getBuffer(value));
+        });
+    };
 
-// Generates the merkle tree 
-MerkleTools.prototype.makeTree = function () {
-    this.tree.isReady = false;
-    var leafCount = this.tree.leaves.length;
-    if (leafCount > 0) { // skip this whole process if there are no leaves added to the tree
-        var levelCount = Math.ceil(Math.log2(leafCount)) + 1;
-        this.tree.levels.unshift(this.tree.leaves);
-        while (this.tree.levels[0].length > 1) {
-            this.tree.levels.unshift(this._calculateNextLevel());
+    // Returns a leaf at the given index
+    this.getLeaf = function (index) {
+        var leafLevelIndex = tree.levels.length - 1;
+        if (index < 0 || index > tree.levels[leafLevelIndex].length - 1) return null; // index is out of array bounds
+
+        return tree.levels[leafLevelIndex][index];
+    };
+
+    // Returns the number of leaves added to the tree
+    this.getLeafCount = function () {
+        return tree.leaves.length;
+    };
+
+    // Returns the ready state of the tree
+    this.getTreeReadyState = function () {
+        return tree.isReady;
+    };    
+
+    // Generates the merkle tree 
+    this.makeTree = function () {
+        tree.isReady = false;
+        var leafCount = tree.leaves.length;
+        if (leafCount > 0) { // skip this whole process if there are no leaves added to the tree
+            var levelCount = Math.ceil(Math.log2(leafCount)) + 1;
+            tree.levels.unshift(tree.leaves);
+            while (tree.levels[0].length > 1) {
+                tree.levels.unshift(_calculateNextLevel());
+            }
         }
-    }
-    this.tree.isReady = true;
-};
+        tree.isReady = true;
+    };
 
-// Returns the merkle root value for the tree
-MerkleTools.prototype.getMerkleRoot = function () {
-    if (!this.tree.isReady) return null;
-    return this.tree.levels[0][0].toString('hex');
-};
+    // Returns the merkle root value for the tree
+    this.getMerkleRoot = function () {
+        if (!tree.isReady) return null;
+        return tree.levels[0][0].toString('hex');
+    };
 
-// Returns the proof for a leaf at the given index as an array of merkle siblings in hex format
-MerkleTools.prototype.getProof = function (index) {
-    if (!this.tree.isReady) return null;
-    var currentRowIndex = this.tree.levels.length - 1;
-    if (index < 0 || index > this.tree.levels[currentRowIndex].length - 1) return null; // the index it out of the bounds of the leaf array
+    // Returns the proof for a leaf at the given index as an array of merkle siblings in hex format
+    this.getProof = function (index) {
+        if (!tree.isReady) return null;
+        var currentRowIndex = tree.levels.length - 1;
+        if (index < 0 || index > tree.levels[currentRowIndex].length - 1) return null; // the index it out of the bounds of the leaf array
 
-    var proof = [];
-    for (var x = currentRowIndex; x > 0; x--) {
+        var proof = [];
+        for (var x = currentRowIndex; x > 0; x--) {
 
-        var currentLevelNodeCount = this.tree.levels[x].length;
-        // skip if this is an odd end node
-        if (index == currentLevelNodeCount - 1 && currentLevelNodeCount % 2 == 1) {
-            index = Math.floor(index / 2);
-            continue;
+            var currentLevelNodeCount = tree.levels[x].length;
+            // skip if this is an odd end node
+            if (index == currentLevelNodeCount - 1 && currentLevelNodeCount % 2 == 1) {
+                index = Math.floor(index / 2);
+                continue;
+            }
+
+            // determine the sibling for the current index and get its value
+            var isRightNode = index % 2;
+            var siblingIndex = isRightNode ? (index - 1) : (index + 1);
+            var sibling = {};
+            var siblingPosition = isRightNode ? 'left' : 'right';
+            var siblingValue = tree.levels[x][siblingIndex].toString('hex');
+            sibling[siblingPosition] = siblingValue;
+
+            proof.push(sibling);
+
+            index = Math.floor(index / 2); // set index to the parent index
         }
 
-        // determine the sibling for the current index and get its value
-        var isRightNode = index % 2;
-        var siblingIndex = isRightNode ? (index - 1) : (index + 1);
-        var sibling = {};
-        var siblingPosition = isRightNode ? 'left' : 'right';
-        var siblingValue = this.tree.levels[x][siblingIndex].toString('hex');
-        sibling[siblingPosition] = siblingValue;
+        return proof;
+    };
 
-        proof.push(sibling);
+    // Takes a proof array, a target hash value, and a merkle root
+    // Checks the validity of the proof and return true or false
+    this.validateProof = function (proof, targetHash, merkleRoot) {
+        targetHash = _getBuffer(targetHash);
+        merkleRoot = _getBuffer(merkleRoot);
+        if (proof.length === 0) return targetHash.toString('hex') == merkleRoot.toString('hex'); // no siblings, single item tree, so the hash should also be the root
 
-        index = Math.floor(index / 2); // set index to the parent index
-    }
-
-    return proof;
-};
-
-// Takes a proof array, a target hash value, and a merkle root
-// Checks the validity of the proof and return true or false
-MerkleTools.prototype.validateProof = function (proof, targetHash, merkleRoot) {
-    targetHash = this._getBuffer(targetHash);
-    merkleRoot = this._getBuffer(merkleRoot);
-    if (proof.length === 0) return targetHash.toString('hex') == merkleRoot.toString('hex'); // no siblings, single item tree, so the hash should also be the root
-
-    var proofHash = targetHash;
-    for (var x = 0; x < proof.length; x++) { 
-        if(proof[x].left === undefined) { // then the sibling is a right node
-            proofHash = this.hashFunction(Buffer.concat([proofHash, this._getBuffer(proof[x].right)]));
-        } else { // the sibling is a left node
-            proofHash = this.hashFunction(Buffer.concat([this._getBuffer(proof[x].left), proofHash]));
-        }        
-    }
-    return proofHash.toString('hex') == merkleRoot.toString('hex');
-};
-
-//////////////////////////////////////////
-// Utility functions
-//////////////////////////////////////////
-
-// Internally, trees are made of nodes containing Buffer values only
-// This helps ensure that leaves being added are Buffers, and will convert hex to Buffer if needed
-MerkleTools.prototype._getBuffer = function (value) {
-    return (value instanceof Buffer ? value : new Buffer(value, 'hex'));
-};
-
-// Calculates the next level of node when building the merkle tree
-// These values are calcalated off of the current highest level, level 0 and will be prepended to the levels array
-MerkleTools.prototype._calculateNextLevel = function () {
-    var nodes = [];
-    var topLevel = this.tree.levels[0];
-    var topLevelCount = topLevel.length;
-    for (var x = 0; x < topLevelCount; x += 2) {
-        if (x + 1 <= topLevelCount - 1) { // concatonate and hash the pair, add to the next level array
-            nodes.push(this.hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]])));
+        var proofHash = targetHash;
+        for (var x = 0; x < proof.length; x++) {
+            if (proof[x].left === undefined) { // then the sibling is a right node
+                proofHash = hashFunction(Buffer.concat([proofHash, _getBuffer(proof[x].right)]));
+            } else { // the sibling is a left node
+                proofHash = hashFunction(Buffer.concat([_getBuffer(proof[x].left), proofHash]));
+            }
         }
-        else { // this is an odd ending node, promote up to the next level by itself
-            nodes.push(topLevel[x]);
-        }
+
+        return proofHash.toString('hex') == merkleRoot.toString('hex');
+    };
+
+    //////////////////////////////////////////
+    // Private Utility functions
+    //////////////////////////////////////////
+
+    // Internally, trees are made of nodes containing Buffer values only
+    // This helps ensure that leaves being added are Buffers, and will convert hex to Buffer if needed
+    function _getBuffer(value) {
+        return (value instanceof Buffer ? value : new Buffer(value, 'hex'));
     }
-    return nodes;
+
+    // Calculates the next level of node when building the merkle tree
+    // These values are calcalated off of the current highest level, level 0 and will be prepended to the levels array
+    function _calculateNextLevel() {
+        var nodes = [];
+        var topLevel = tree.levels[0];
+        var topLevelCount = topLevel.length;
+        for (var x = 0; x < topLevelCount; x += 2) {
+            if (x + 1 <= topLevelCount - 1) { // concatonate and hash the pair, add to the next level array
+                nodes.push(hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]])));
+            }
+            else { // this is an odd ending node, promote up to the next level by itself
+                nodes.push(topLevel[x]);
+            }
+        }
+        return nodes;
+    }
 };
 
 module.exports = MerkleTools;
--- test/main.js
+++ test/main.js
@@ -8,12 +8,27 @@ var bLeft = new Buffer('a292780cc748697cb499fdcc8cb89d835609f11e502281dfe3f6690b
 var bRight = new Buffer('cb4990b9a8936bbc137ddeb6dcab4620897b099a450ecdc5f3e86ef4b3a7135c', 'hex');
 var mRoot = crypto.createHash('sha256').update(Buffer.concat([bLeft, bRight])).digest();
 
-describe("make tree with addLeaves", function () {
+
+describe("make tree with addLeaves hex", function () {
 
     var merkleTools = new merkletools();
     merkleTools.addLeaves([
         'a292780cc748697cb499fdcc8cb89d835609f11e502281dfe3f6690b1cc23dcb',
         'cb4990b9a8936bbc137ddeb6dcab4620897b099a450ecdc5f3e86ef4b3a7135c'
+    ]);
+    merkleTools.makeTree();
+
+    it("merkle root value should be correct", function () {
+        assert.equal(merkleTools.getMerkleRoot(), mRoot.toString('hex'));
+    });
+
+});
+
+describe("make tree with addLeaves buffers", function () {
+
+    var merkleTools = new merkletools();
+    merkleTools.addLeaves([
+        bLeft, bRight
     ]);
     merkleTools.makeTree();
 
@@ -34,8 +49,8 @@ describe("reset tree", function () {
     merkleTools.resetTree();
 
     it("tree should be empty", function () {
-        assert.equal(merkleTools.tree.leaves.length, 0);
-        assert.equal(merkleTools.tree.isReady, false);
+        assert.equal(merkleTools.getLeafCount(), 0);
+        assert.equal(merkleTools.getTreeReadyState(), false);
     });
 });
 
@@ -61,6 +76,38 @@ describe("make tree with addLeaf buffers", function () {
 
     it("merkle root value should be correct", function () {
         assert.equal(merkleTools.getMerkleRoot(), mRoot.toString('hex'));
+    });
+
+});
+
+describe("make tree with 1 leaf", function () {
+
+    var merkleTools = new merkletools();
+    merkleTools.addLeaves([
+        'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'
+    ]);
+    merkleTools.makeTree();
+
+    it("merkle root value should be correct", function () {
+        assert.equal(merkleTools.getMerkleRoot(), 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb');
+    });
+
+});
+
+describe("make tree with 5 leaves", function () {
+
+    var merkleTools = new merkletools();
+    merkleTools.addLeaves([
+        'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb',
+        '3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d',
+        '2e7d2c03a9507ae265ecf5b5356885a53393a2029d241394997265a1a25aefc6',
+        '18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4',
+        '3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea'
+    ]);
+    merkleTools.makeTree();
+
+    it("merkle root value should be correct", function () {
+        assert.equal(merkleTools.getMerkleRoot(), 'd71f8983ad4ee170f8129f1ebcdd7440be7798d8e1c80420bf11f1eced610dba');
     });
 
 });
@@ -92,7 +139,21 @@ describe("proof right node", function () {
     });
 
 }); 
-describe("validate bad proof", function () {
+
+describe("proof one node", function () {
+
+    var merkleTools = new merkletools();
+    merkleTools.addLeaf(bLeft);
+    merkleTools.makeTree();
+    var proof = merkleTools.getProof(0);
+
+    it("proof array should be correct", function () {
+        assert.deepEqual(proof, []);
+    });
+
+}); 
+
+describe("validate bad proof 2 leaves", function () {
 
     var merkleTools = new merkletools();
     merkleTools.addLeaf(bLeft);
@@ -108,14 +169,40 @@ describe("validate bad proof", function () {
 });
 
 
-describe("validate good proof", function () {
+describe("validate bad proof 5 leaves", function () {
 
     var merkleTools = new merkletools();
-    merkleTools.addLeaf(bLeft);
-    merkleTools.addLeaf(bRight);
+    merkleTools.addLeaves([
+        'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb',
+        '3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d',
+        '2e7d2c03a9507ae265ecf5b5356885a53393a2029d241394997265a1a25aefc6',
+        '18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4',
+        '3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea'
+    ]);
     merkleTools.makeTree();
-    var proof = merkleTools.getProof(1);
-    var isValid = merkleTools.validateProof(proof, bRight, mRoot);
+    var proof = merkleTools.getProof(3);
+    var isValid = merkleTools.validateProof(proof, 'badc3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4', 'd71f8983ad4ee170f8129f1ebcdd7440be7798d8e1c80420bf11f1eced610dba');
+
+
+    it("proof should be invalid", function () {
+        assert.equal(isValid, false);
+    });
+    
+});
+
+describe("validate good proof 5 leaves", function () {
+
+    var merkleTools = new merkletools();
+    merkleTools.addLeaves([
+        'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb',
+        '3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d',
+        '2e7d2c03a9507ae265ecf5b5356885a53393a2029d241394997265a1a25aefc6',
+        '18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4',
+        '3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea'
+    ]);
+    merkleTools.makeTree();
+    var proof = merkleTools.getProof(0);
+    var isValid = merkleTools.validateProof(proof, 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb', 'd71f8983ad4ee170f8129f1ebcdd7440be7798d8e1c80420bf11f1eced610dba');
 
 
     it("proof should be valid", function () {
