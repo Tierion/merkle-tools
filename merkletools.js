5f65a15b0257c0f1daccbac0dafe4da3ad59a066
Glenn Rempe <glenn@tierion.com>
2017-09-11 17:45:33 -0700

    Apache 2.0 LICENSE

--- merkletools.js
+++ merkletools.js
@@ -1,5 +1,17 @@
 'use strict'
 
+/* Copyright 2017 Tierion
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*     http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 var sha3512 = require('js-sha3').sha3_512
 var sha3384 = require('js-sha3').sha3_384
 var sha3256 = require('js-sha3').sha3_256
